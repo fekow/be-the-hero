e99680c7f1cc85950eb3546555d96e791078c02d
Fernando Binkowski <binkowskideandrade@gmail.com>
2020-06-14 16:52:20 -0300

    Added mobile frontend for user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import "./global.css";
+import Routes from "./routes"
+// JSX (JavaScript e XML)
 
 function App() {
   return (
-    <h1>Hello, World!</h1>
+    <Routes />
   );
 }
 
 export default App;
+
+// import React, {useState} from 'react';
+  // //useState é um array com o valor e uma função que atualiza o valor- para usar seta o valor inicial com ()
+  // const [counter, setCounter] = useState(0);
+
+  // function increment () {
+  //   setCounter(counter + 1);
+  // }
+
+  // return (
+  //   <div>
+  //     <Header>Contador: {counter}</Header>
+  //     <button onClick={increment}>Incrementar</button>
+  //   </div>
